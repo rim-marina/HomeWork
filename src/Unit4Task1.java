c594bbfac91450be582005d25f05e8c1260f2edf
Marina <rim-marina@yandex.ru>
2021-07-20 17:02:49 +0300

    commit

---
+++ src/Unit4Task1.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Unit4Task1 {
+    public static void main(String[] args) {
+        // Поcчитать сумму четных элементов стоящих на главной диагонали.
+        Matrix m = new Matrix();
+        int[][] matrix = m.createMatrix();
+        int sum = 0;
+
+        for(int i = 0; i < matrix.length; i++){
+            int j = i;
+            if(matrix[i][j] % 2 == 0){
+                sum += matrix[i][j];
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
